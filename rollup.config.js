155475ffbd9e0a525bbd4e5688c0cf51de216855
Dutta, Jaibatrik <Jaibatrik.Dutta@in.pega.com>
2017-07-09 23:32:33 +0530

    AOT Compatible, not distributing JS as of now

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+
+export default {
+  entry: 'dist/index.js',
+  dest: 'dist/bundles/angular4-social-login.js', // output a single application bundle
+  sourceMap: false,
+  format: 'iife',
+  moduleName: 'Angular4SocialLogin',
+  onwarn: function (warning) {
+    // Skip certain warnings
+
+    // should intercept ... but doesn't in some rollup versions
+    if (warning.code === 'THIS_IS_UNDEFINED') { return; }
+
+    // console.warn everything else
+    console.warn(warning.message);
+  },
+  plugins: [
+    nodeResolve({ jsnext: true, module: true }),
+    commonjs({
+      include: 'node_modules/rxjs/**',
+    }),
+    uglify()
+  ]
+};
